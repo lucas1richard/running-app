0216bf207458c4d3fc3381e182e46b9e6013e7ac
Richard <lucas1richard@gmail.com>
2024-06-27 09:27:48 -0400

    fix(activities-service): properly store ZonesCache

--- packages/activities-service/src/persistence/heartzones/index.js
+++ packages/activities-service/src/persistence/heartzones/index.js
@@ -8,15 +8,13 @@ const ZonesCache = require('./model-zones-cache')
 
 const createHeartZonesCacheOnce = async (activityId, heartZoneId, times) => {
   await ZonesCache.create({
-    where: {
-      seconds_z1: times[0],
-      seconds_z2: times[1],
-      seconds_z3: times[2],
-      seconds_z4: times[3],
-      seconds_z5: times[4],
-      activityId,
-      heartZoneId,
-    },
+    seconds_z1: times[0],
+    seconds_z2: times[1],
+    seconds_z3: times[2],
+    seconds_z4: times[3],
+    seconds_z5: times[4],
+    activityId,
+    heartZoneId,
   }, {
     ignoreDuplicates: true,
   });
