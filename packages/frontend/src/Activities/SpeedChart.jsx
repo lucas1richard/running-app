0bda5c4136158663eeaf670b5bc43ba28f24c319
Richard <lucas1richard@gmail.com>
2024-06-27 16:37:07 -0400

    feat(frontend): enhance SpeedChart

--- packages/frontend/src/Activities/SpeedChart.jsx
+++ packages/frontend/src/Activities/SpeedChart.jsx
@@ -1,46 +1,83 @@
 import dayjs from 'dayjs';
 import Highcharts from 'highcharts';
 import HighchartsReact from 'highcharts-react-official';
-import { convertMetersToMiles, convertMetricSpeedToMPH, convertSpeedToPace, getDuration } from '../utils';
+import { convertMetricSpeedToMPH } from '../utils';
 import { useMemo } from 'react';
 
-const SpeedChart = ({ activities: actProp }) => {
+const seriesDefaultConfig = {
+  states: {
+    inactive: {
+      opacity: 1
+    },
+    hover: {
+      lineWidthPlus: 0,
+    },
+  },
+  fillOpacity: 0.3,
+  lineWidth: 4,
+  animation: false,
+};
 
-  const activities = actProp.filter(({ start_date }) => dayjs(start_date).isAfter(dayjs().subtract(1, 'year')));
+const yAxisDefaultConfig = {
+  crosshair: true,
+  gridLineWidth: 1,
+  minorGridLineWidth: 1,
+  minorTickInterval: 'auto',
+  tickInterval: 1,
+  gridLineColor: 'rgba(0,0,0,0.4)',
+  minorGridLineColor: 'rgba(0,0,0,0.1)',
+  height: '50%',
+  opposite: false,
+}
+
+const SpeedChart = ({ activities: actProp }) => {
+  const activities = useMemo(
+    () => actProp.filter(({ start_date }) => dayjs(start_date).isAfter(dayjs().subtract(1, 'year'))).reverse(),
+    [actProp]
+  );
 
   /** @type {Highcharts.Options} */
   const options = useMemo(() => ({
     chart: {
       type: 'line',
       height: 400,
-      // width: 600,
+      zooming: {
+        type: 'x',
+      },
+    },
+    legend: {
+      enabled: false,
     },
     title: {
-      text: 'Average Speed Over Time',
+      text: '&nbsp;',
       align: 'left',
       margin: 20,
       x: 30
     },
-    zooming: 'x',
     series: [
       {
         name: 'Speed',
-        data: activities.map(({ start_date, average_speed }) => [new Date(start_date).getTime(), convertMetricSpeedToMPH(average_speed)]).reverse(),
+        data: activities.map(({ start_date, average_speed }) => [new Date(start_date).getTime(), convertMetricSpeedToMPH(average_speed)]),
         yAxis: 0,
-        color: 'black',
-        lineWidth: 2,
-        animation: false,
+        color: {
+          linearGradient: { x1: 0, x2: 0, y1: 0, y2: 1 },
+          stops: [[0, '#0f0'], [1, '#f00']],
+        },
+        
+        ...seriesDefaultConfig,
       },
       {
         name: 'Distance',
-        data: activities.map(({ start_date, distance }) => [new Date(start_date).getTime(), convertMetersToMiles(distance)]).reverse(),
-        yAxis: 0,
+        data: activities.map(({ start_date, distance_miles }) => [new Date(start_date).getTime(), distance_miles]),
+        yAxis: 1,
         dateFormat: 'MMM DD',
-        color: 'blue',
-        lineWidth: 2,
-        animation: false,
+        color: {
+          linearGradient: { x1: 0, x2: 0, y1: 0, y2: 1 },
+          stops: [[0, 'rgba(0,0,255,1)'], [1, 'rgba(0,0,55,1)']],
+        },
+        ...seriesDefaultConfig,
       },
-    ].filter(Boolean),
+    ],
     xAxis: {
       crosshair: true,
       type: 'datetime',
@@ -54,10 +91,9 @@ const SpeedChart = ({ activities: actProp }) => {
     },
     yAxis: [
       {
-        crosshair: true,
-        gridLineWidth: 1,
+        ...yAxisDefaultConfig,
         title: {
-          text: 'Speed',
+          text: 'Avg Speed',
           style: {
             color: 'black'
           }
@@ -65,27 +101,48 @@ const SpeedChart = ({ activities: actProp }) => {
         labels: {
           format: '{value} mph',
           style: {
-            color: 'black'
-          }
+            color: 'black',
+          },
         },
-        opposite: false,
+      },
+      { // Secondary yAxis
+        ...yAxisDefaultConfig,
+        top: '50%',
+        title: {
+          text: 'Distance',
+          style: {
+            color: 'blue',
+          },
+        },
+        labels: {
+          format: '{value} mi',
+          style: {
+            color: 'blue',
+          },
+        },
+        opposite: true,
       }
     ],
     tooltip: {
+      useHTML: true,
       positioner: function () {
         return {
-            // right aligned
-            x: this.chart.chartWidth - this.label.width,
-            y: 10 // align to title
+          // right aligned
+          x: this.chart.chartWidth - this.label.width,
+          y: 0 // align to title
         };
-    },
+      },
       formatter: function () {
+        const index = this.point.index;
+        const activity = activities[index];
         return `
-          <b>${dayjs(this.x).format('MMM DD')}</b>
-          <br>
-          ${this.y.toFixed(2)} ${this.series.name === 'Speed' ? 'mph' : 'miles'}
-          <br>
-          ${getDuration(convertSpeedToPace(convertMetricSpeedToMPH(this.y))).join(' ')} /mi
+          <div class="text-right dls-white-bg pad border-1">
+            <b>${dayjs(activity.start_date).format('MMM DD')}</b>
+            <br />
+            ${activities[index].distance_miles.toFixed(2)} miles
+            <br /> 
+            ${convertMetricSpeedToMPH(activities[index].average_speed).toFixed(2)} mph
+          </div>
         `;
       },
       borderWidth: 0,
@@ -94,7 +151,7 @@ const SpeedChart = ({ activities: actProp }) => {
       headerFormat: '',
       shadow: false,
       style: {
-          fontSize: '18px'
+        fontSize: '18px'
       },
     },
   }), [activities]);
