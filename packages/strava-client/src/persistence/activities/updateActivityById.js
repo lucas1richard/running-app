bac53cfeb14edcf75c80e838c313abadd2b716e2
Richard <lucas1richard@gmail.com>
2024-06-27 16:42:39 -0400

    chore(strava-client): remove unused

--- packages/strava-client/src/persistence/activities/updateActivityById.js
+++
@@ -1,7 +0,0 @@
-const Activity = require('./model-activities');
-
-const updateActivityById = async (id, fields) => {
-  return Activity.update(fields, { where: { id } });
-};
-
-module.exports = updateActivityById;
--- packages/strava-client/src/persistence/segments/findAthleteSegmentsByActivityId.js
+++
@@ -1,7 +0,0 @@
-const AthleteSegment = require('./model-athlete-segments');
-
-const findAthleteSegmentsByActivityId = async (activityId) => {
-  return AthleteSegment.findAllByActivityId(activityId)
-};
-
-module.exports = findAthleteSegmentsByActivityId;
