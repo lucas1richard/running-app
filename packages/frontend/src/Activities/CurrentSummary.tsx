694629ca2f817e950adb1eae9e08a9236f97a12f
Richard <lucas1richard@gmail.com>
2024-07-01 23:27:36 -0400

    chore(frontend): Convert CurrentSummary to TypeScript

--- packages/frontend/src/Activities/CurrentSummary.tsx
+++ packages/frontend/src/Activities/CurrentSummary.tsx
@@ -1,23 +1,27 @@
 import React from 'react';
-import dayjs, { utc } from 'dayjs';
+import dayjs from 'dayjs';
 import { convertMetersToMiles } from '../utils';
 
-dayjs.extend(utc);
+interface Activitiy {
+  start_date: string;
+  start_date_local: string;
+  distance: number;
+}
 
-const findRecent = (allActivities, numDays) => {
+const findRecent = (allActivities: Activitiy[], numDays: number) => {
   const currentDateUTC = dayjs.utc();
   return allActivities.filter(({ start_date }) => {
-    const activityDateUTC = dayjs(start_date);
-    return Math.floor((currentDateUTC - activityDateUTC) / (24 * 60 * 60 * 1000)) < numDays;
+    const activityDateUTC = dayjs(start_date).utc().diff(currentDateUTC);
+    return Math.floor(activityDateUTC / (24 * 60 * 60 * 1000)) < numDays;
   });
 };
 
-const findSameYear = (allActivities) => {
+const findSameYear = (allActivities: Activitiy[]) => {
   const currentDate = dayjs();
   return allActivities.filter(({ start_date_local }) => dayjs(start_date_local).isSame(currentDate, 'year'));
 };
 
-const sumDistance = (activities) => {
+const sumDistance = (activities: Activitiy[]) => {
   const meters = activities.reduce((acc, { distance }) => acc + distance, 0);
   return convertMetersToMiles(meters);
 };
