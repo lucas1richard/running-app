1ffa5118ecef0b37f8fd1e974fd10c489dfb20b0
Richard <lucas1richard@gmail.com>
2024-12-25 12:49:39 -0500

    feat(apiSaga): fallback to hash action if key not provided

--- packages/frontend/src/sagas/apiSaga.ts
+++ packages/frontend/src/sagas/apiSaga.ts
@@ -1,17 +1,22 @@
 import { call, put } from 'redux-saga/effects';
+import { hash } from 'ohash';
 import {
   setApiErrorAct,
   setApiLoadingAct,
   setApiSuccessAct,
 } from '../reducers/apiStatus-actions';
+import type { AsyncAction } from '../types';
 
-const makeApiSaga = (cb) => function* apiSaga(action) {
+type CB =(...args: any[]) => any;
+
+const makeApiSaga = (cb: CB) => function* apiSaga(action: AsyncAction) {
+  const loadingKey = action.key || hash(action);
   try {
-    yield put(setApiLoadingAct(action.key));
-    yield call(cb, action);
-    yield put(setApiSuccessAct(action.key));
+    yield put(setApiLoadingAct(loadingKey));
+    yield call<CB>(cb, action);
+    yield put(setApiSuccessAct(loadingKey));
   } catch (err) {
-    yield put(setApiErrorAct(action.key));
+    yield put(setApiErrorAct(loadingKey));
   }
 }
 
