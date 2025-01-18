fd2ebda95a9c5d3816b7754b364948e6b65ca6ef
Richard <lucas1richard@gmail.com>
2025-01-18 15:22:13 -0500

    feat(go-server): add go server to packages

---
+++ packages/activities-go-server/apiserver/apiserver.go
@@ -0,0 +1,80 @@
+package apiserver
+
+import (
+	"context"
+	"errors"
+	"net/http"
+	"time"
+
+	"github.com/gorilla/mux"
+	"github.com/lucas1richard/activities-go-server/storage"
+	"github.com/sirupsen/logrus"
+)
+
+var defaultStopTimeout = time.Second * 30
+
+type APIServer struct {
+	addr    string
+	storage *storage.Storage
+}
+
+type Endpoint struct {
+	handler EndpointFunc
+}
+
+type EndpointFunc func(w http.ResponseWriter, req *http.Request) error
+
+func (e Endpoint) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	if err := e.handler(w, req); err != nil {
+		logrus.WithError(err).Error("could not process request")
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("internal server error"))
+	}
+}
+
+func NewAPIServer(addr string, storage *storage.Storage) (*APIServer, error) {
+	if addr == "" {
+		return nil, errors.New("addr cannot be blank")
+	}
+
+	return &APIServer{
+		addr:    addr,
+		storage: storage,
+	}, nil
+}
+
+// Start starts a server with a stop channel
+func (s *APIServer) Start(stop <-chan struct{}) error {
+	srv := &http.Server{
+		Addr:    s.addr,
+		Handler: s.router(),
+	}
+
+	go func() {
+		logrus.WithField("addr", srv.Addr).Info("startingg server")
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			logrus.Fatalf("listen: %s\n", err)
+		}
+	}()
+
+	<-stop
+	ctx, cancel := context.WithTimeout(context.Background(), defaultStopTimeout)
+	defer cancel()
+
+	logrus.WithField("timeout", defaultStopTimeout).Info("stopping server")
+	return srv.Shutdown(ctx)
+}
+
+func (s *APIServer) router() http.Handler {
+	router := mux.NewRouter()
+
+	router.HandleFunc("/", s.defaultRoute)
+	// router.Methods("POST").Path("/items").Handler(Endpoint{s.createItem})
+	router.Methods("GET").Path("/items").Handler(Endpoint{s.listItems})
+	return router
+}
+
+func (s *APIServer) defaultRoute(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("Hello World\n\nhello again"))
+}
---
+++ packages/activities-go-server/go.mod
@@ -0,0 +1,18 @@
+module github.com/lucas1richard/activities-go-server
+
+go 1.23.5
+
+require (
+	github.com/gorilla/mux v1.8.1
+	github.com/sirupsen/logrus v1.9.3
+	github.com/urfave/cli/v2 v2.27.5
+)
+
+require (
+	filippo.io/edwards25519 v1.1.0 // indirect
+	github.com/cpuguy83/go-md2man/v2 v2.0.5 // indirect
+	github.com/go-sql-driver/mysql v1.8.1 // indirect
+	github.com/russross/blackfriday/v2 v2.1.0 // indirect
+	github.com/xrash/smetrics v0.0.0-20240521201337-686a1a2994c1 // indirect
+	golang.org/x/sys v0.0.0-20220715151400-c0bba94af5f8 // indirect
+)
